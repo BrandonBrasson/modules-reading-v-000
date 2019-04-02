390634dfbb5efa80e4a3289b248e9f8ac044be2e
Brandon Brasson <rocksong622@gmail.com>
2019-04-02 18:34:40 +0000

    Automatically backed up by Learn

--- spec/dance_spec.rb
+++ spec/dance_spec.rb
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
- puts describe 'Dance module' do
+  describe 'Dance module' do
 =======
-puts describe 'Dance module' do
+ describe 'Dance module' do
 >>>>>>> 19aab4b67aa7c696e39243588f2628ba8dddd3cd
   let(:dummy_class) { Class.new { include Dance }.new }
 
