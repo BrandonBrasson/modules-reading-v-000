bb28c52691b26953c6b1edb5f8571e0088d74108
Brandon Brasson <rocksong622@gmail.com>
2019-04-02 18:37:45 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,23 +1,17 @@
+module Dance
+  def twirl
+    "I'm twirling!"
+  end
 
+  def jump
+    "Look how high I'm jumping!"
+  end
 
+  def pirouette
+    "I'm doing a pirouette"
+  end
 
-
-def twirl
-<<<<<<< HEAD
-  "I'm twirling!"
-=======
-  "I'm twirling!" 
->>>>>>> 19aab4b67aa7c696e39243588f2628ba8dddd3cd
-end
-
-def jump
-  "Look how high I'm jumping!"
-end
-
-def pirouette
-  "I'm doing a pirouette"
-end
-
-def take_a_bow
-  "Thank you, thank you. It was a pleasure to dance for you all."
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end
 end
